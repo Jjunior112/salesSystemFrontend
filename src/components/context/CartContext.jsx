efc5b97724cc80a00fe15de13aeff67b21f6a539
jjunior112 <juarezgcjr@gmail.com>
2025-08-15 05:22:48 -0300

    context do carrinho de compras

---
+++ src/components/context/CartContext.jsx
@@ -0,0 +1,49 @@
+import { createContext, useState, useEffect } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState(() => {
+        return JSON.parse(localStorage.getItem("cart")) || [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }, [cart]);
+
+    const increaseToCart = (product) => {
+        setCart((prevCart) => {
+            const existing = prevCart.find(item => item.id === product.id);
+            if (existing) {
+                return prevCart.map(item =>
+                    item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+                );
+            }
+            return [...prevCart, { ...product, quantity: 1 }];
+        });
+    };
+    const decreaseToCart = (product) => {
+        setCart((prevCart) => {
+            const existing = prevCart.find(item => item.id === product.id);
+            if (existing) {
+                return prevCart.map(item =>
+
+                    item.id === product.id && item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item
+                );
+            }
+            return [...prevCart, { ...product, quantity: product.quantity - 1 }];
+        });
+    };
+
+    const removeFromCart = (id) => {
+        setCart(prevCart => prevCart.filter(item => item.id !== id));
+    };
+
+    const clearCart = () => setCart([]);
+
+    return (
+        <CartContext.Provider value={{ cart, decreaseToCart, increaseToCart, removeFromCart, clearCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+};
